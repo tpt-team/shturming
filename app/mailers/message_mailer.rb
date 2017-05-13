212e1fcc6afd6e8587818641cb579549d6491e03
YaroslavTupitskyi <jaroslav.typitsky@gmail.com>
2017-05-13 13:38:24 +0300

    Settings mailer

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,7 +1,7 @@
 class MessageMailer < ApplicationMailer
   def morning_email(body, user)
     @body = body
-    @name = user[:name]
+    @email = user[:email]
     mail to: user[:email], subject: 'Good morning'
   end
 end
