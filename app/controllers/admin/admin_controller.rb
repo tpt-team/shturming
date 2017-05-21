a2358a64e0706ab240c73f705db8597dfcc9d6b2
YaroslavTupitskyi <jaroslav.typitsky@gmail.com>
2017-05-21 21:54:06 +0300

    Basic authenticate for admin

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,5 +1,5 @@
 class Admin::AdminController < ApplicationController
   layout 'admin'
 
-  http_basic_authenticate_with name: 'admin', password: 'secret'
+  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
 end
