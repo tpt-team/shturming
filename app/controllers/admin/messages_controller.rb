642df93af0f68eaceac024fde46b19ae0e8d5986
YaroslavTupitskyi <jaroslav.typitsky@gmail.com>
2017-05-21 22:32:45 +0300

    Add ability to update messages

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -1,5 +1,18 @@
 class Admin::MessagesController < Admin::AdminController
   def index
-    @messages = Message.all
+    @messages = Message.all.paginate(page: params[:page])
+  end
+
+  def update
+    MessageMailer.morning_email(message.body, message.recipient).deliver
+    message.success!
+  rescue => e
+    Rails.logger.error(e.message)
+  end
+
+  private
+
+  def message
+    @message ||= Message.find(params[:id])
   end
 end
