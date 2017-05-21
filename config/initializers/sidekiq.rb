cd46cb4f8d0eb69e4b6c01f9cd9c60b3c5170561
YaroslavTupitskyi <jaroslav.typitsky@gmail.com>
2017-05-21 19:14:54 +0300

    Small change

--- config/initializers/sidekiq.rb
+++
@@ -1,8 +0,0 @@
-require 'sidekiq/web'
-require './lib/dynamic_fetch'
-
-redis_url = ENV['REDIS_URL'] || 'redis://localhost:6379'
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: redis_url }
-end
