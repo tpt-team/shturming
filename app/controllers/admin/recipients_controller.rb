5a3eb8391e489d9a6c1d9bd1bf20673683c86f9d
YaroslavTupitskyi <jaroslav.typitsky@gmail.com>
2017-05-27 15:09:17 +0300

    Add ability to destroy recipients for admin

--- app/controllers/admin/recipients_controller.rb
+++ app/controllers/admin/recipients_controller.rb
@@ -1,5 +1,15 @@
 class Admin::RecipientsController < Admin::AdminController
   def index
-    @recipients = Recipient.all
+    @recipients = Recipient.all.paginate(page: params[:page], per_page: 10)
+  end
+
+  def destroy
+    recipient.destroy
+  end
+
+  private
+
+  def recipient
+    @recipient = Recipient.find(params[:id])
   end
 end
